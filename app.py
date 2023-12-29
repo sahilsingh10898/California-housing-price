d88458a39dd79696f10563961f04c790e157f783
sahilsingh10898 <137412499+sahilsingh10898@users.noreply.github.com>
2023-12-29 19:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+##creating the web application,(creating a web page where user give all the inputs)
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House Price Prediction is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
